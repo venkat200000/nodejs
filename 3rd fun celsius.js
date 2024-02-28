1ce5298c0b96b71c0ad62d01fae92861c8abc663
venkat200000 <160600270+venkat200000@users.noreply.github.com>
2024-02-28 14:21:24 +0530

    Add files via upload

---
+++ 3rd fun celsius.js
@@ -0,0 +1,9 @@
+//temp                                                                                                                                                                                                                                                                                               
+let celsius=22;
+let farhenit;
+function a()
+{
+     let a =(celsius*9/5+32);
+    return a;
+}
+console.log( 'farhenit',a());
